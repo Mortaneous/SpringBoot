54b5900f0a70a2ade10c4bf6ff6a23af883928e1
Mortaneous <mortaneous.git@gmail.com>
2018-11-09 03:30:12 +0800

    Tutorial: Spring Boot Quick Start 16

--- src/main/java/com/mortaneous/springboot/topic/TopicController.java
+++ src/main/java/com/mortaneous/springboot/topic/TopicController.java
@@ -3,18 +3,25 @@ package com.mortaneous.springboot.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/topics")
 public class TopicController {
 
-	@RequestMapping("/topics")
+	@GetMapping
 	public List<Topic> getAllTopics() {
 		return Arrays.asList(
 				new Topic("java", "Java", "All about Java"),
 				new Topic("cpp", "C++", "Everything C++"),
 				new Topic("html", "HTML", "HTML pages")
 		);
+	}
+	
+	@GetMapping("/id")
+	public Topic getTopic() {
+		return new Topic("aTopic", "A Topic", "topic topic");
 	}
 }
