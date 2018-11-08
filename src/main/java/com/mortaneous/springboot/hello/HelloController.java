cb70edff70aedc275bfa68230ed2bf519143e675
Mortaneous <mortaneous.git@gmail.com>
2018-11-09 02:29:14 +0800

    Initial commit

---
+++ src/main/java/com/mortaneous/springboot/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.mortaneous.springboot.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping
+	public String greet() {
+		return "Hello, World!";
+	}
+}
