4b9df8a23d8689251006e34ac6e5a70ee42c0a47
Mortaneous <mortaneous.git@gmail.com>
2018-11-10 03:51:16 +0800

    Changed the project description

--- src/main/java/com/mortaneous/springboot/courseapi/hello/HelloController.java
+++ src/main/java/com/mortaneous/springboot/courseapi/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.mortaneous.springboot.hello;
+package com.mortaneous.springboot.courseapi.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
